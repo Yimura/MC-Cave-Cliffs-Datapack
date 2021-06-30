206e0687b126b6d4b8c829b8695a57e9e13a648f
Yimura <andreas.maerten@scarlet.be>
2021-06-30 17:50:30 +0200

    fix(Startup): Spawn server directly in bash
    Fixes SIGINT/SIGTERM not propagating to java server

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 FILE=server.jar
 if [ ! -f "$FILE" ]; then
     echo "Server jar does not exist in container root directory, moving..."
     mv /$FILE ./
     mv /fabric-server-launch.jar ./
 fi
-java -Xmx6G -Xms6G -jar fabric-server-launch.jar
+exec java -Xmx6G -Xms6G -jar fabric-server-launch.jar
